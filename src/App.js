9cdfa6ef0dbb3ddeed1a4d1cd19dffc0d6eb3fa1
mightymaggot55 <48097201+mightymaggot55@users.noreply.github.com>
2020-10-08 19:11:24 +0100

    uiFix

--- src/App.js
+++ src/App.js
@@ -11,31 +11,58 @@ import { Colour } from './ui/components/elements'
 
 const { Content } = Layout;
 
-const contentStyles = {
-    background: Colour().black,
-    color: '#0a0001',
-    padding: 40
+const AppWrapper = styled.div`
+  display: flex;
+  flex-flow: column;
+  align-items: flex-start;
+  height: 100vh;
+`
+const Body = styled.div`
+  width: 60%;
+  margin: auto; 
+`
+
+const HeaderWrapper = styled.div`
+  ${({ theme }) => theme.flexRowNoWrap}
+  width: 100%;
+  justify-content: space-between;
+`
+/*This is the page background color*/
+const BodyWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  justify-content: flex-start;
+  align-items: center;
+  flex: 1;
+  overflow: auto;
+  min-height: 10vh;
+  background-position: 0px 0vh;
+  background-color: #0a0001;
+  background-repeat: no-repeat;
 }
-
-
+`
 const App = () => {
     return (
         <Router>
             <div>
                 <ContextProvider>
-                    <Layout style={{ height: "100vh", background: Colour().black }}>
-                        <Headbar />
-                        <Content style={contentStyles}>                            
+                    <AppWrapper>
+                        <HeaderWrapper>
+                            <Headbar />
+                        </HeaderWrapper>
+                        <BodyWrapper>
+                            <Body>
                                 <Switch>
                                     <Route path="/" exact component={SimpleSwap} />
                                     <Route path="/swap" exact component={SimpleSwap} />
                                 </Switch>
-                        </Content> 
-                        <Footer/>
-                    </Layout>
+                            </Body>
+                        </BodyWrapper>
+                    </AppWrapper>
                 </ContextProvider>
             </div>
-        </Router >
+        </Router>
     );
 }
 
